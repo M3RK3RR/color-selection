fb222b76b3a3ff1906487fee98734d89af516537
M3RK3RR <korabelynicov23@gmail.com>
2024-11-01 20:39:29 +0500

    realized by M3RK3RR

---
+++ app.js
@@ -0,0 +1,25 @@
+const ul = document.querySelector('ul');
+const section = document.querySelector('section');
+
+ul.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName === 'LI'){
+        const color  = target.dataset.color;
+        const text = target.textContent;
+        const textColor = target.dataset.text ?? 'white'
+
+        section.style.background = `#${color}`;
+        section.style.color = `${textColor}`;
+        section.textContent = text;
+        section.dataset.color = color;
+
+        alert(`You clicked on ${event.target.textContent}`)
+    }
+})
+
+section.addEventListener('click', (event) => {
+    const color = event.target.dataset.color;
+    navigator.clipboard.writeText(color).then(() => {
+        alert(event.target.textContent + ' copied!')
+    })
+})
